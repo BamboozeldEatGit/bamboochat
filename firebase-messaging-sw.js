e6fae3be515b0719a28cee29259a16131b77e5b4
BamboozeldEatGit <113732239+BamboozeldEatGit@users.noreply.github.com>
2023-09-16 15:15:39 -0500

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,10 +1,10 @@
-   if ("serviceWorker" in navigator && "PushManager" in window) {
-        navigator.serviceWorker
-            .register("/firebase-messaging-sw.js")
-            .then((registration) => {
-                console.log("Service Worker registered with scope:", registration.scope);
-            })
-            .catch((error) => {
-                console.error("Service Worker registration failed:", error);
-            });
-    }
+if ("serviceWorker" in navigator && "PushManager" in window) {
+    navigator.serviceWorker
+        .register("/firebase-messaging-sw.js")
+        .then((registration) => {
+            console.log("Service Worker registered with scope:", registration.scope);
+        })
+        .catch((error) => {
+            console.error("Service Worker registration failed:", error);
+        });
+}
